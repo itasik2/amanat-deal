8759c8ef37a0471da238a430d1aa34a7699edad6
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-08-18 14:33:04 +0500

    feat(api): wire Prisma into deals module

--- apps/api/src/modules/deals/deals.module.ts
+++ apps/api/src/modules/deals/deals.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { DealsController } from './deals.controller';
 import { DealsService } from './deals.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DealsController],
   providers: [DealsService]
 })
