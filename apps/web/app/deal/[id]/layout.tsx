7c2a68e9925f14e2e3a8165199c47a97f8225ead
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-08-21 13:18:42 +0500

    refactor: render deal workspace from page

--- apps/web/app/deal/[id]/layout.tsx
+++ apps/web/app/deal/[id]/layout.tsx
@@ -1,10 +1,3 @@
-import { DealExtras } from './DealExtras';
-
 export default function DealLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {children}
-      <DealExtras />
-    </>
-  );
+  return children;
 }
