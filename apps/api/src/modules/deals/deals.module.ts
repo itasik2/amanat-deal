5bcca6ac3ef21e248e9cda39981bb96a72b80b15
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-09-22 16:29:23 +0500

    feat: export deal access authorization service

--- apps/api/src/modules/deals/deals.module.ts
+++ apps/api/src/modules/deals/deals.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { DealAccessService } from './deal-access.service';
 import { DealInvitationsController } from './deal-invitations.controller';
 import { DealsController } from './deals.controller';
 import { DealsService } from './deals.service';
@@ -8,6 +9,7 @@ import { SecureInvitationsService } from './secure-invitations.service';
 @Module({
   imports: [AuthModule],
   controllers: [DealsController, DealInvitationsController],
-  providers: [DealsService, SecureInvitationsService]
+  providers: [DealsService, SecureInvitationsService, DealAccessService],
+  exports: [DealAccessService]
 })
 export class DealsModule {}
