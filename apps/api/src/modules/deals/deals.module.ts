f67034398734c695dd6e1480f97754a5810642b6
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-09-22 16:41:16 +0500

    feat: register inspection automation

--- apps/api/src/modules/deals/deals.module.ts
+++ apps/api/src/modules/deals/deals.module.ts
@@ -4,12 +4,19 @@ import { DealAccessService } from './deal-access.service';
 import { DealInvitationsController } from './deal-invitations.controller';
 import { DealsController } from './deals.controller';
 import { DealsService } from './deals.service';
+import { InspectionAutomationController } from './inspection-automation.controller';
+import { InspectionAutomationService } from './inspection-automation.service';
 import { SecureInvitationsService } from './secure-invitations.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [DealsController, DealInvitationsController],
-  providers: [DealsService, SecureInvitationsService, DealAccessService],
+  controllers: [DealsController, DealInvitationsController, InspectionAutomationController],
+  providers: [
+    DealsService,
+    SecureInvitationsService,
+    DealAccessService,
+    InspectionAutomationService
+  ],
   exports: [DealAccessService]
 })
 export class DealsModule {}
